c5f4f70276f79d116466c466fff3a899c3dd5d1f
Min Idzelis <min123@gmail.com>
2023-06-22 16:26:20 +0000

    Move scripts and dynamically link up services.d/cont-init.d

--- root/installBinary.sh
+++ root/installBinary.sh
@@ -8,6 +8,14 @@ addVarToConf "version" "${TAG}"
 addVarToConf "plex_build" "${PLEX_BUILD}"
 addVarToConf "plex_distro" "${PLEX_DISTRO}"
 
+# Setup common services
+echo "Creating services"
+mkdir -p /etc/services.d/plex ; ln -s /etc/plex/service.plex.run /etc/services.d/plex/run
+mkdir -p /etc/cont-init.d
+ln -s /etc/plex/plex-first-run /etc/cont-init.d/000-plex-first-run
+ln -s /etc/plex/plex-hw-transcode-and-connected-tuner /etc/cont-init.d/010-plex-hw-transcode-and-connected-tuner
+ln -s /etc/plex/plex-update /etc/cont-init.d/020-plex-update
+
 if [ -n "${URL}" ]; then
   echo "Attempting to install from URL: ${URL}"
   installFromRawUrl "${URL}"
