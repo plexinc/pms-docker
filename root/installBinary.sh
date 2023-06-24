709a6debb03a43b54f9db24ca4d032c54f30d151
Min Idzelis <min123@gmail.com>
2023-06-24 17:23:53 +0000

    Backwards compat. FORCE_UPDATE var

--- root/installBinary.sh
+++ root/installBinary.sh
@@ -11,6 +11,7 @@ fi
 
 . /plex-common.sh
 
+addVarToConf "tag" "${TAG}"
 addVarToConf "plex_build" "${PLEX_BUILD}"
 addVarToConf "plex_distro" "${PLEX_DISTRO}"
 
@@ -23,7 +24,7 @@ ln -s /etc/plex/plex-hw-transcode-and-connected-tuner /etc/cont-init.d/010-plex-
 if [ -n "${URL}" ]; then
   echo "Attempting to install from URL: ${URL}"
   installFromRawUrl "${URL}"
-elif [ "${TAG,,}" = "autoupdate" ]; then
+elif [ "${TAG,,}" = "autoupdate" ] || [ "${TAG,,}" = "beta" ] || [ "${TAG,,}" = "public" ]; then
   echo "AUTOUPDATE requested, skipping download and install, setting up cron service and scheduling plex-update"
   mkdir -p /etc/services.d/cron ; ln -s /etc/plex/service.cron.run /etc/services.d/cron/run
   # Specify bash as shell, add S6 commands to PATH
@@ -31,8 +32,8 @@ elif [ "${TAG,,}" = "autoupdate" ]; then
   echo "$(env | grep PATH):/command" >> /etc/crontab
   # Specify cron job: be nice to Plex servers - space out updates at 4:00am over 30m window
   echo "0 4 * * * root perl -le 'sleep rand 30' ; /etc/plex/plex-update > /proc/1/fd/1 2>&1" >> /etc/crontab
-  echo "Adding plex-install to cont-init.d"
-  ln -s /etc/plex/plex-install /etc/cont-init.d/099-plex-install
+  echo "Adding plex-startup to cont-init.d"
+  ln -s /etc/plex/plex-startup /etc/cont-init.d/099-plex-startup
 else
   # This pre-installs the specified version in TAG into this docker image.
   remoteVersion=
@@ -46,5 +47,6 @@ else
   
   echo "Attempting to install: ${remoteVersion}"
   installFromUrl "${remoteFile}"
+  # delete unnecessary installer
   rm -rf /config/install
 fi
